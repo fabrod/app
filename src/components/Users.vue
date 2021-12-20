327072b345ded6db608e475183fca207ebcea751
fabrod <fabrod035@gmail.com>
2021-12-20 13:53:39 -0500

    indent code

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,54 +1,51 @@
 <template>
-<fragment>
-    <div class='container'>
-    <h3>Users:</h3>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">City</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="user in users" v-bind:key="user.id">
-      <th scope="row">{{user.id}}</th>
-      <td>{{user.name}}</td>
-      <td>{{user.email}}</td>
-      <td>{{user.address.city}}</td>
-    </tr>
-  </tbody>
-</table>
+  <fragment>
+    <div class="container">
+      <h3>Users:</h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">City</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" v-bind:key="user.id">
+            <th scope="row">{{ user.id }}</th>
+            <td>{{ user.name }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.address.city }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-</fragment>
-  
+  </fragment>
 </template>
 
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  name: 'Users',
+  name: "Users",
   data() {
-      return{
-          users: null,
-      };
+    return {
+      users: null,
+    };
   },
-  created: function() {
-      axios
-      .get('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
-          this.users = res.data;
-      })
-  }
-}
+  created: function () {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.users = res.data;
+    });
+  },
+};
 </script>
 
 
 <style>
 h3 {
-  margin-top: 5px;  
+  margin-top: 5px;
 }
 </style>
